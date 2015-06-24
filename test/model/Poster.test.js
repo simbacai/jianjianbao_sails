55d46056d7e56bdd7d69f57364cd7ec33f2f8ec5
simbacai <simbacai@yahoo.com>
2015-06-24 22:39:54 +0800

    open user read permission for all registered user

--- test/model/Poster.test.js
+++ test/model/Poster.test.js
@@ -1,10 +1,10 @@
 require('should');
 
 describe('Poster', function() {
-it ('should not be empty', function(done) {
-  Poster.find().exec(function(err, posters) {
-    posters.length.should.be.eql(fixtures['poster'].length);
-    done();
-  });
-});
+	it ('should not be empty', function(done) {
+	  Poster.find().exec(function(err, posters) {
+	    posters.length.should.be.eql(fixtures['poster'].length);
+	    done();
+	  });
+	});
 });
