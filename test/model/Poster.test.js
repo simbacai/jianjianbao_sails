1c6b765d152da9bdaf0cb4ff5fe6e4cb03b41da9
simbacai <simbacai@yahoo.com>
2015-06-24 17:37:00 +0800

    downgrade should version for compatability reason

--- test/model/Poster.test.js
+++ test/model/Poster.test.js
@@ -1,9 +1,9 @@
-var should = require('should');
+require('should');
 
 describe('Poster', function() {
 it ('should not be empty', function(done) {
   Poster.find().exec(function(err, posters) {
-    //posters.length.should.be.eql(fixtures['poster'].length);
+    posters.length.should.be.eql(fixtures['poster'].length);
     done();
   });
 });
