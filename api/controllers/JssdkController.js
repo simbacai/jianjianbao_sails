ccdb16fc3ae5d09afaf78a7ca0033ac8a2ce2edb
simbacai <simbacai@yahoo.com>
2015-07-10 14:16:13 +0800

    Finish getJsSign API

--- api/controllers/JssdkController.js
+++ api/controllers/JssdkController.js
@@ -5,13 +5,36 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var weixin = require('weixin-node');
+var co = require('co'); 
+
 var sign = require('../services/jssign');
 
 module.exports = {
 	getsign : function (req, res) {
-		var ret = sign(req.query.url);
-		sails.log(ret);
-		res.ok(ret);
+
+		co(function *() {
+    var token = yield weixin.getToken('wxcd3e2f8024ba7f49', 'e90587f697dade57be212bec97da173d');
+    console.log('token:', token);
+
+    var ticket = yield weixin.getTicket(token.access_token);
+    console.log('ticket:', ticket);
+    //return ticket;
+
+     var sign = yield weixin.sign(ticket.ticket, req.query.url);
+     console.log('sign:', sign);
+
+     return sign;
+	  })
+	  .then (function (sign) {
+	    //console.log(sign);
+	    res.ok(sign);
+	  })
+	  .catch (function (err) {
+	  	sails.log.error(err);
+	  	res.serverError(err);
+	  });
+
 	}
 };
 
