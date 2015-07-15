9daca483af44d10d69a4abcc49738f1469cf317d
simbacai <simbacai@yahoo.com>
2015-07-15 15:42:10 +0800

    Remove the unnecessary log for jssdkcontroller

--- api/controllers/JssdkController.js
+++ api/controllers/JssdkController.js
@@ -13,14 +13,10 @@ module.exports = {
 
 		co(function *() {
     var token = yield weixin.getToken('wxcd3e2f8024ba7f49', 'e90587f697dade57be212bec97da173d');
-    console.log('token:', token);
 
     var ticket = yield weixin.getTicket(token.access_token);
-    console.log('ticket:', ticket);
-    //return ticket;
 
      var sign = yield weixin.sign(ticket.ticket, req.query.url);
-     console.log('sign:', sign);
 
      return sign;
 	  })
