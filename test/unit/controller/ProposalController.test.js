9791b85f9e9c7ae89d24d45eb9d8b58ea2fb9047
echunca <simba.cai@ericsson.com>
2015-07-18 21:45:11 +0800

    add 8 ProposalController test cases

--- test/unit/controller/ProposalController.test.js
+++ test/unit/controller/ProposalController.test.js
@@ -1,48 +1,9 @@
 var request = require('supertest');
 
-describe('PosterController', function() {
+describe('ProposalController', function() {
 
-  describe('#post() without localpassport', function() {
-    it('should redirect to /auth/weixin', function (done) {
-      request(sails.hooks.http.app)
-        .post('/poster')
-        .send({ subject: 'testsubject', body: 'testbody', tipAmount: "100" })
-        .expect(302)
-        .expect('location','/auth/weixin', done);
-    });
-  });
-
-  describe('#get() without localpassport', function() {
-    it('should redirect to /auth/weixin', function (done) {
-      request(sails.hooks.http.app)
-        .get('/poster/1')
-        .expect(302)
-        .expect('location','/auth/weixin', done);
-    });
-  });
-
-  describe('#put() without localpassport', function() {
-    it('should redirect to /auth/weixin', function (done) {
-      request(sails.hooks.http.app)
-        .put('/poster/1')
-        .send({body: 'updatebody'})
-        .expect(302)
-        .expect('location','/auth/weixin', done);
-    });
-  });
-
-  describe('#delete() without localpassport', function() {
-    it('should redirect to /auth/weixin', function (done) {
-      request(sails.hooks.http.app)
-        .delete('/poster/1')
-        .expect(302)
-        .expect('location','/auth/weixin', done);
-    });
-  });
-
- //Todo Actually need to assert the body content
-  describe('#post() with localpassport', function() {
-    it('should post succesfully', function (done) {
+  describe('#Create Poster for ProposalController testing', function() {
+    it('should create succesfully', function (done) {
       request(sails.hooks.http.app)
         .post('/poster')
         .set('Authorization', 'Basic cG9zdGVyb3duZXI6dGVzdGpqMTIz')
@@ -57,80 +18,110 @@ describe('PosterController', function() {
     });
   });
 
-  describe('#get() with localpassport', function() {
-    it('should get succesfully', function (done) {
+  describe('#post() without localpassport', function() {
+    it('should redirect to /auth/weixin', function (done) {
       request(sails.hooks.http.app)
-        .get('/poster/1')
-        .set('Authorization', 'Basic cG9zdGVyb3duZXI6dGVzdGpqMTIz')
-        .expect('Content-Type', /json/) 
-        .expect(200)
-        .end(function(err, res) {
-          if (err) return done(err);
-          sails.log.silly(res);
-          done();
-        }) 
+        .post('/proposal')
+        .send({ poster: '2', content: 'testproposal'})
+        .expect(302)
+        .expect('location','/auth/weixin', done);
     });
   });
 
-  describe('#put() with localpassport', function() {
-    it('should put succesfully', function (done) {
+  describe('#get() without localpassport', function() {
+    it('should redirect to /auth/weixin', function (done) {
       request(sails.hooks.http.app)
-        .put('/poster/1')
-        .set('Authorization', 'Basic cG9zdGVyb3duZXI6dGVzdGpqMTIz')
-        .send({body: 'updatebody'})
-        .expect('Content-Type', /json/) 
-        .expect(200)
-        .end(function(err, res) {
-          if (err) return done(err);
-          sails.log.silly(res);
-          done();
-        }) 
+        .get('/proposal?poster=2')
+        .expect(302)
+        .expect('location','/auth/weixin', done);
+    });
+  });
+    
+  describe('#put() without localpassport', function() {
+    it('should redirect to /auth/weixin', function (done) {
+      request(sails.hooks.http.app)
+        .put('/proposal/1')
+        .send({content: 'updateProposal'})
+        .expect(302)
+        .expect('location','/auth/weixin', done);
     });
   });
 
-  describe('#post image with localpassport', function() {
-    it('should post image succesfully', function (done) {
+/*
+  describe('#delete() without localpassport', function() {
+    it('should redirect to /auth/weixin', function (done) {
       request(sails.hooks.http.app)
-        .post('/poster/image/1')
-        .set('Authorization', 'Basic cG9zdGVyb3duZXI6dGVzdGpqMTIz')
-        .attach('image', 'test/fixtures/ab.jpg')
-        .expect(200)
-        .end(function(err, res) {
-          if (err) return done(err);
-          sails.log.silly(res);
-          done();
-        }) 
-    });
-  });
-  
-  //There is undefined error of fileadaptor, to be debugged
-  /*
-  describe('#get image with localpassport', function() {
-    it('should get image succesfully', function (done) {
-      request(sails.hooks.http.app)
-        .get('/poster/image/1')
-        .set('Authorization', 'Basic cG9zdGVyb3duZXI6dGVzdGpqMTIz')
-        .end(function(err, res) {
-          if (err) return done(err);
-          sails.log.silly(res);
-          done();
-        }) 
+        .delete('/proposal/1')
+        .expect(302, done);
+        //.expect('location','/auth/weixin', done);  //To Debug
     });
   });
 */
 
-  describe('#delete() with localpassport', function() {
-    it('should delete succesfully', function (done) {
+  //Testing Poster Owner's proposal Actions
+  describe('#post() with localpassport', function() {
+    it('should post successfully', function (done) {
       request(sails.hooks.http.app)
-        .delete('/poster/1')
+        .post('/proposal')
         .set('Authorization', 'Basic cG9zdGVyb3duZXI6dGVzdGpqMTIz')
-        .expect('Content-Type', /json/) 
+        .send({ poster: '2', content: 'testproposal'})
+        .expect(201)
+        .end(function(err, res) {
+          if (err) return done(err);
+          sails.log.silly(res);
+          done();
+        })
+
+    });
+  });
+
+  describe('#get() with localpassport', function() {
+    it('should get successfully', function (done) {
+      request(sails.hooks.http.app)
+        .get('/proposal/1')
+        .set('Authorization', 'Basic cG9zdGVyb3duZXI6dGVzdGpqMTIz')
         .expect(200)
         .end(function(err, res) {
           if (err) return done(err);
           sails.log.silly(res);
           done();
-        }) 
+        })
+
     });
   });
+
+  describe('#put() with localpassport', function() {
+    it('should put successfully', function (done) {
+      request(sails.hooks.http.app)
+        .put('/proposal/1')
+        .set('Authorization', 'Basic cG9zdGVyb3duZXI6dGVzdGpqMTIz')
+        .send({content: 'testproposal'})
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          sails.log.silly(res);
+          done();
+        })
+
+    });
+  });
+
+  describe('#delete() with localpassport', function() {
+    it('should delete successfully', function (done) {
+      request(sails.hooks.http.app)
+        .delete('/proposal/1')
+        .set('Authorization', 'Basic cG9zdGVyb3duZXI6dGVzdGpqMTIz')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          sails.log.silly(res);
+          done();
+        })
+
+    });
+  });
+  //Testing Poster Non-owner's proposal Actions
+
+
+
 });
