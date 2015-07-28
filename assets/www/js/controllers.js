7c391d52a0b618dd90214ed6e54b3f55c9de2cfd
cai chun <simbacai@yahoo.com>
2015-07-28 17:13:34 +0800

    add chat html

--- assets/www/js/controllers.js
+++ assets/www/js/controllers.js
@@ -186,10 +186,21 @@ app
     $scope.userEntryClick = function(index) {
 
         $ionicModal.fromTemplateUrl('/www/templates/user.html', {
-          scope: $scope//.poster.users[index]
+          scope: $scope
         }).then(function(modal) {
             $scope.displayUser = $scope.poster.users[index];
             $scope.userModal = modal;
+            modal.show();
+        });
+    }
+
+    $scope.chatClick = function(friend) {
+
+        $ionicModal.fromTemplateUrl('/www/templates/chat.html', {
+          scope: $scope
+        }).then(function(modal) {
+            $scope.friend = friend;
+            $scope.chatModal = modal;
             modal.show();
         });
     }
